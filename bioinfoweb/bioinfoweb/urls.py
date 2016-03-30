934d536e32bf7d6e0d93e3bbd59c41d7dcc507d9
david <david.bour@hologic.com>
2016-03-30 16:58:03 -0700

    Changing template colors

--- bioinfoweb/bioinfoweb/urls.py
+++ bioinfoweb/bioinfoweb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from portal import urls as portal_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^portal/', include(portal_urls)),
 ]
