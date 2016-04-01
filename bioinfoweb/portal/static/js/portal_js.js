e7c43ab430437c20a74ac3df88915eaf1c87bb4f
david <david.bour@hologic.com>
2016-04-01 16:15:32 -0700

    Changing frontpage layout with terminal look

--- bioinfoweb/portal/static/js/portal_js.js
+++ bioinfoweb/portal/static/js/portal_js.js
@@ -1,15 +1,10 @@
 // Uses typed.js to write out a terminal message
 $(function() {
 	$("#typedJSContainer").typed({
-		strings: ["<b style='font-size: xx-large'>Welcome</b>", 
-		"<b style='font-size: xx-large'>Bioinformatics</b> \
-		<br/> <em style='font-size: large;'>Core Technolgies Group</em> \
-		<br/> \
-		<br/> \
-		What would you like to do today...?  "],
+		strings: ["Welcome to the Bioinformatics Homepage <br/> What would you like to do today?"],
 		typeSpeed: 0,
 		backDelay: 300,
 		showCursor: true,
-		cursorChar: "|"
+		cursorChar: "_"
 		});
 	});
