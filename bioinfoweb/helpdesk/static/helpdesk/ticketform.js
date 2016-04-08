4638b755ae4a5b3ff2143b966a94d96743b5f67d
david <david.bour@hologic.com>
2016-04-07 17:13:35 -0700

    Added a way to keep the queue custom forms from showing when django refreshes on invalid inputs

--- bioinfoweb/helpdesk/static/helpdesk/ticketform.js
+++ bioinfoweb/helpdesk/static/helpdesk/ticketform.js
@@ -1,7 +1,7 @@
-$(document).ready(function()
-	{	
+$(document).ready(function() {	
 
-		var sentence;
+		// Keeps track of which form is showing
+		var current_selected = "default";
 
 		// Assign the IDs of the custom fields for different categories
 		var sequencing_form = $("#seq_request_form");
@@ -15,31 +15,67 @@ $(document).ready(function()
 		// categories. The forms then get written into a text and entered into the
 		// description box, so the fields are not actually saved into the database individually
 
+		// Holds the selected dropdown of Queue
+		var selected;
+
 		$("#id_queue").change(function() {
 
-			var selected = $("#id_queue option:selected").text();
+			selected = $("#id_queue option:selected").text();
 
 			if ((selected) === "Training Requests") {
+				current_selected = training_form;
 				training_form.show();
 				sequencing_form.hide();
 			} else if ((selected === "Sequencing Requests")) {
+				current_selected = sequencing_form;
 				training_form.hide();
 				sequencing_form.show();
 			} else if ((selected === "In-Silico Analysis")) {
+				current_selected = "default";
 				training_form.hide();
 				sequencing_form.hide();
 			} else {
+				current_selected = "default";
 				training_form.hide();
 				sequencing_form.hide();
 			}
 			
 		});	
 
+
+		// ________________________________________________________________
+		//
+		// Temporary FIX for the DJANGO refresh on failure
+		// so it would show the error and still render the correct forms
+		selected = $("#id_queue option:selected").text();
+
+			if ((selected) === "Training Requests") {
+				current_selected = training_form;
+				training_form.show();
+				sequencing_form.hide();
+			} else if ((selected === "Sequencing Requests")) {
+				current_selected = sequencing_form;
+				training_form.hide();
+				sequencing_form.show();
+			} else if ((selected === "In-Silico Analysis")) {
+				current_selected = "default";
+				training_form.hide();
+				sequencing_form.hide();
+			} else {
+				current_selected = "default";
+				training_form.hide();
+				sequencing_form.hide();
+			}
+		// _______________________________________________________________
+
+
 		// Appends the extra fields into description of issue before sending off
+
 		$("#forpresubmit").click(function() {
-			sentence = $("#seq_request_form").serialize();
-			$("#id_body").val(sentence);
-		})
+			if (current_selected != "default") {
+				var sentence = $(current_selected).serialize();
+				$("#id_body").val(sentence);
+			};
+		});
 
-
-	})
+});
