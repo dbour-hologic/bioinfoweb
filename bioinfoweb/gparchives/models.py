7662fbe54245267ff9d539e4670e11277393d83f
david <david.bour@hologic.com>
2016-04-18 16:21:33 -0700

    Added models to add files for archives, need to update views

--- bioinfoweb/gparchives/models.py
+++ bioinfoweb/gparchives/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import datetime
+
+# Creates a range of years to use for year selection
+YEAR_CHOICES = [(yr,yr) for yr in range(1960, datetime.datetime.now().year+1)] 
 
 # Create your models here.
+class Documents(models.Model):
+	document = models.FileField(
+		upload_to='archives'
+	)
+
+	title = models.CharField(
+		max_length=200,
+		null=True,
+		blank=True,
+	)
+
+	author = models.CharField(
+		max_length=200,
+		null=True,
+		blank=True,
+	)
+
+	year = models.IntegerField(
+		max_length=4,
+		choices=YEAR_CHOICES,
+	)
+
+	citation = models.CharField(
+		max_length=400,
+		null=True,
+		blank=True,
+	)
+
+	keywords = models.CharField(
+		max_length=200,
+		null=True,
+		blank=True,
+	)
