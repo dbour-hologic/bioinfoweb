3cb9d14f9963207b745506301547972abb70c44f
db_development <dbour@dbdevelmentsmbp.attlocal.net>
2016-04-20 22:44:32 -0700

    Added gparchives to admin interface. Modified article archive jsonfile to include citations. Modified all base templates to include links to gparchives

--- bioinfoweb/gparchives/admin.py
+++ bioinfoweb/gparchives/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from gparchives.models import Documents
 # Register your models here.
+
+class Documents_Admin(admin.ModelAdmin):
+
+	model = Documents
+
+	list_display = ('document_title', 'document_path',)
+
+	def document_title(self, obj):
+		return ("%s" % obj.title)
+	document_title.short_description = "Document Title"
+
+	def document_path(self, obj):
+		return ("%s" % obj.document)
+	document_path.short_description = "File Path"
+
+admin.site.register(Documents, Documents_Admin)
