12443018fed52fa52b996b763695635a00734f71
david <david.bour@hologic.com>
2016-05-05 13:16:59 -0700

    Added bug report to admin

--- bioinfoweb/reportbug/admin.py
+++ bioinfoweb/reportbug/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from reportbug.models import ReportBug
+
+class report_bug_admin(admin.ModelAdmin):
+
+	model = ReportBug
+	list_display = ('bug_title',)
+
+	def bug_title(self, obj):
+		return ("%s" % (obj.bug_title))
+	bug_title.short_description = "Bug Title"
+
+admin.site.register(ReportBug, report_bug_admin)
