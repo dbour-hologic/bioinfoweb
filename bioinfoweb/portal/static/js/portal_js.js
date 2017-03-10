d19c3e58c055b565b5ddce5847e7bd1eb9ea584b
david.bour@hologic.com <david.bour@hologic.com>
2017-03-10 11:31:02 -0800

    Added modification to CSS for portal and modified the base html

--- bioinfoweb/portal/static/js/portal_js.js
+++ bioinfoweb/portal/static/js/portal_js.js
@@ -8,3 +8,26 @@ $(function() {
 		cursorChar: "_"
 		});
 	});
+
+$('#nav').affix({
+      offset: {
+        top: $('.full').height()
+      }
+});	
+
+$("#gotointro").on("click",function(event) {
+  if (this.hash !== "")
+  {
+    event.preventDefault();
+    // Store the Hash
+    var hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+  }
+});
