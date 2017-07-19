82c31ab228d633e7b251db0921a62623c4fc319c
David Bour <david.bour@hologic.com>
2017-07-19 11:07:10 -0700

    Added admin page for limits and worklist. Also changed the models verbose name to be non-plural

--- bioinfoweb/pqanalysis/admin.py
+++ bioinfoweb/pqanalysis/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Worklist, Limits
 
-# Register your models here.
+
+class WorklistAdmin(admin.ModelAdmin):
+    pass
+
+
+class LimitsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Worklist, WorklistAdmin)
+admin.site.register(Limits, LimitsAdmin)
+
